e72e5c556bf70c6a2cae23b8754e76659914222f
fazaio <faza6028@gmail.com>
2024-02-12 15:07:06 +0700

    hello world

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <section class="container mx-auto mt-10 h-screen">
+    <div>
+      <div>
+        <iframe class="w-full rounded" height="400"
+          src="https://www.youtube-nocookie.com/embed/RRgLZ66NCmE?si=-eW9Ofw5EUJxc8i3" title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen></iframe>
+      </div>
+      <div class="py-10">
+        <h1 class="text-3xl text-gray-300">DIRTY VOTE - Full Movie (OFFICIAL)</h1>
+        <p class="mt-5 text-gray-400 text-justify">Dirty Vote dokumenter dibintangi oleh tiga ahli hukum tata negara. Mereka adalah
+          Zainal Arifin Mochtar, Bivitri
+          Susanti, dan Feri Amsari. Ketiganya mengungkap berbagai instrumen kekuasaan telah digunakan untuk tujuan
+          memenangkan pemilu dan merusak tatanan demokrasi.
+          Penggunaan infrastruktur kekuasaan yang kuat, tanpa malu-malu dipertontonkan secara telanjang demi
+          mempertahankan status quo. Bentuk-bentuk kecurangannya diurai dengan analisa hukum tata negara.</p>
+        <p class="mt-2 text-gray-300 text-xs">Diunggah: <span class="font-bold">11 Februari 2024</span> . Durasi: <span class="font-bold">1 Jam 57 Menit 21 Detik.</span></p>
+        <div class="mt-10 text-gray-300">Bagikan :
+          <div class="flex text-xs">
+            <ShareNetwork class="m-2 border px-4 py-2 rounded-md" network="twitter" url="https://dirty-vote.web.app"
+              title="🎥 Streaming film dokumenter DIRTY VOTE-Full Movie (OFFICIAL) tanpa iklan - 🍿 https://dirty-vote.web.app/ "
+              hashtags="dirtyvote,kopagsamnidu">
+              Twitter/X
+            </ShareNetwork>
+            <ShareNetwork class="m-2 border px-4 py-2 rounded-md" network="facebook" url="https://dirty-vote.web.app"
+              title="DIRTY VOTE - Full Movie (OFFICIAL)"
+              description="🎥 Streaming film dokumenter DIRTY VOTE-Full Movie (OFFICIAL) tanpa iklan - 🍿 https://dirty-vote.web.app/ "
+              hashtags="dirtyvote,kopagsamnidu">
+              Facebook
+            </ShareNetwork>
+            <ShareNetwork class="m-2 border px-4 py-2 rounded-md" network="whatsapp" url="https://dirty-vote.web.app"
+              title="DIRTY VOTE - Full Movie (OFFICIAL)"
+              description="🎥 Streaming film dokumenter DIRTY VOTE-Full Movie (OFFICIAL) tanpa iklan - 🍿 https://dirty-vote.web.app/ "
+              hashtags="dirtyvote,kopagsamnidu">
+              Whatsapp
+            </ShareNetwork>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="sticky p-2 bottom-0 w-full text-white text-center text-sm">❤️ by Aux | #Dirtyvote, #kopagsamNidu | 🇮🇩</footer>
+</template>
+
+<style>
+body {
+  @apply bg-slate-900;
+  @apply relative;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import VueSocial from "vue-social-sharing";
+
+createApp(App).use(VueSocial).mount('#app')
